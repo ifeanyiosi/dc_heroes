8568078f33f780a03be30a416ba972614b969800
Ifeanyi Osi-Okeke <107844542+ifeanyiSMG@users.noreply.github.com>
2022-09-08 01:30:18 +0100

    .....

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,12 @@
+import {GiHamburgerMenu} from 'react-icons/gi'
+
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between bg-[#790252] p-4">
       <span>Ifeanyi's App</span>
+      <div>
+        <GiHamburgerMenu />
+      </div>
       <div className="gap-8 hidden sm:flex justify-center text-white pr-8">
         <a href="">Home</a>
         <a href="">About</a>
